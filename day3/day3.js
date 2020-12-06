57d746792eeaad1e492d63867dbbbfdda783bc0e
Maxime <maxime.jme@gmail.com>
2020-12-06 19:02:55 +0100

    add part 2 of day 3

--- day3/day3.js
+++ day3/day3.js
@@ -324,20 +324,42 @@ const input = [
     ".#..#.#.#..#....#.............."
 ];
 
-let lineLength = input[0].length;
-let x = 0;
-let y = 0;
-let numberOfTrees = 0;
+function part1(right, down) {
+    let lineLength = input[0].length;
+    let x = 0;
+    let y = 0;
+    let numberOfTrees = 0;
 
-while(y < input.length) {
-    const currentX = x % lineLength;
+    while(y < input.length) {
+        const currentX = x % lineLength;
 
-    if(input[y][currentX] === "#") {
-        numberOfTrees++;
+        if(input[y][currentX] === "#") {
+            numberOfTrees++;
+        }
+
+        x += right;
+        y += down;
     }
 
-    x += 3;
-    y += 1;
+    return numberOfTrees;
 }
 
-console.log("Arbres: " + numberOfTrees);
+function part2() {
+    const slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ]
+
+    let result = 1;
+
+    slopes.forEach(slope => {
+        result *= part1(slope[0], slope[1]);
+    })
+
+    return result;
+}
+
+console.log(part2());
