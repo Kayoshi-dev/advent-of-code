b995d6635af6a7563438039022520f2007474a0e
Maxime <maxime.jme@gmail.com>
2020-12-11 01:45:38 +0100

    improve algo and clean code
    doesn't work, steel don't know why after 5 hours

--- day5/day5.java
+++ day5/day5.java
@@ -1,95 +1,95 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class day5 {
+    static int[] rows = {0, 127};
+    static int row;
+
+    static int[] columns = {0, 7};
+    static int column;
+
+    static ArrayList<Integer> computedResult = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         File file = new File("input.txt");
-        int[] rows = {0, 127};
-        int row = 0;
-        int[] columns = {0, 7};
-        int column = 0;
-        int index = 0;
-        ArrayDeque<Integer> computedResultDeque = new ArrayDeque<>();
+        BufferedReader br = new BufferedReader(new FileReader(file));
         String line;
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        while ((line = br.readLine()) != null) {
+            char[] letter = line.toCharArray();
 
-        while((line = br.readLine()) != null) {
-            char[] chars = line.toCharArray();
-            for (char letter : chars) {
-                System.out.println(letter);
-
-                if(index < 6) {
-                    checkRow(letter, rows);
-                } else if(index == 6) {
-                    Arrays.sort(rows);
-                    row = rows[0];
-                    System.out.println("La rangée est : " + row);
-                } else if(index > 6 && index < 9) {
-                    checkColumn(letter, columns);
-                } else if(index == 9) {
-                    Arrays.sort(columns);
-                    column = columns[1];
-                    System.out.println("La colonne est : " + column);
-                }
-                index++;
-                System.out.println(index);
+            for (int i = 0; i < letter.length; i++) {
+                determineSeat(letter, i);
             }
-            index = 0;
 
-            System.out.println("ROW:" + row + "COL:" + column );
+            rows[0] = 0;
+            rows[1] = 127;
+            row = 0;
 
-            computeResult(row, column, computedResultDeque);
+            columns[0] = 0;
+            columns[1] = 7;
+            column = 0;
         }
 
-        int[] computedResult = new int[computedResultDeque.size()];
-        int count = 0;
-        for(Iterator<Integer> i = computedResultDeque.iterator(); i.hasNext();) {
-            computedResult[count++] = i.next();
-        }
-        System.out.println("------------------");
-
-        System.out.println(computedResultDeque);
-
-        System.out.println("------------------");
-
-
-        Arrays.sort(computedResult);
-
-        System.out.println("Valeur maximale : " + computedResult[computedResult.length - 1]);
-
-        System.out.println("Rangée : " + row);
-        System.out.println("Colonne : " + column);
+        int idMax = Collections.max(computedResult);
+        System.out.println(idMax);
     }
 
-    private static void checkRow(char letter, int[] rows) {
+    private static void determineSeat(char[] letter, int index) {
+        if(index < 6) {
+            checkRows(letter[index]);
+        } else if (index == 6) {
+            Arrays.sort(rows);
+
+            if(letter[index] == 'F') {
+                row = rows[0];
+            } else {
+                row = rows[1];
+            }
+
+//            System.out.println(row);
+//            System.out.println(Arrays.toString(rows));
+        } else if (index < 9) {
+            checkColumns(letter[index]);
+        } else if (index == 9) {
+            Arrays.sort(columns);
+            if(letter[index] == 'R') {
+                column = columns[1];
+            } else {
+                column = columns[0];
+            }
+
+            System.out.println(Arrays.toString(columns));
+        }
+
+        addToArray();
+    }
+
+    private static void checkRows(char letter) {
+        System.out.println(Arrays.toString(rows) + letter);
+
         if(letter == 'F') {
-            rows[1] = (int)Math.ceil((rows[0] + rows[1])) / 2;
+            rows[1] = (int)Math.floor((rows[0] + rows[1])) / 2;
         } else if (letter == 'B') {
-            rows[0] = (int)Math.floor((rows[0] + rows[1])) / 2;
+            rows[0] = (int)Math.ceil((rows[0] + rows[1])) / 2;
         }
-
-        System.out.println("Printing rows :");
-        System.out.println(Arrays.toString(rows));
     }
 
-    private static void checkColumn(char letter, int[] columns) {
-        if(letter == 'L') {
-            columns[1] = (int)Math.ceil((columns[0] + columns[1])) / 2;
-        } else if (letter == 'R') {
-            columns[0] = (int)Math.floor((columns[0] + columns[1])) / 2;
-        }
+    private static void checkColumns(char letter) {
+        System.out.println(Arrays.toString(columns) + letter);
 
-        System.out.println("Printing columns :");
-        System.out.println(Arrays.toString(columns));
+        if(letter == 'R') {
+            columns[1] = (int)Math.floor((columns[0] + columns[1])) / 2;
+        } else if (letter == 'L') {
+            columns[0] = (int) Math.ceil((columns[0] + columns[1])) / 2;
+        }
     }
 
-    private static void computeResult(int row, int col, ArrayDeque<Integer> computedResultDeque) {
-        int res = ((row * 8) + col);
+    private static void addToArray() {
+        int rowID = row * 8 + column;
 
-        computedResultDeque.push(res);
+        computedResult.add(rowID);
     }
 }
