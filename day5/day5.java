cd785f7fc1a34392363962dac00604fdef73e48f
Maxime <maxime.jme@gmail.com>
2020-12-11 18:15:03 +0100

    fix my poop code

--- day5/day5.java
+++ day5/day5.java
@@ -4,13 +4,13 @@ import java.io.FileReader;
 import java.util.*;
 
 public class day5 {
-    static int[] rows = {0, 127};
-    static int row;
+    static double[] rows = {0, 127};
+    static double row;
 
-    static int[] columns = {0, 7};
-    static int column;
+    static double[] columns = {0, 7};
+    static double column;
 
-    static ArrayList<Integer> computedResult = new ArrayList<>();
+    static ArrayList<Double> computedResult = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         File file = new File("input.txt");
@@ -33,7 +33,7 @@ public class day5 {
             column = 0;
         }
 
-        int idMax = Collections.max(computedResult);
+        double idMax = Collections.max(computedResult);
         System.out.println(idMax);
     }
 
@@ -48,9 +48,6 @@ public class day5 {
             } else {
                 row = rows[1];
             }
-
-//            System.out.println(row);
-//            System.out.println(Arrays.toString(rows));
         } else if (index < 9) {
             checkColumns(letter[index]);
         } else if (index == 9) {
@@ -60,35 +57,29 @@ public class day5 {
             } else {
                 column = columns[0];
             }
-
-            System.out.println(Arrays.toString(columns));
         }
 
         addToArray();
     }
 
     private static void checkRows(char letter) {
-        System.out.println(Arrays.toString(rows) + letter);
-
         if(letter == 'F') {
-            rows[1] = (int)Math.floor((rows[0] + rows[1])) / 2;
+            rows[1] = Math.floor((rows[0] + rows[1]) / 2);
         } else if (letter == 'B') {
-            rows[0] = (int)Math.ceil((rows[0] + rows[1])) / 2;
+            rows[0] = Math.ceil((rows[0] + rows[1]) / 2);
         }
     }
 
     private static void checkColumns(char letter) {
-        System.out.println(Arrays.toString(columns) + letter);
-
         if(letter == 'R') {
-            columns[1] = (int)Math.floor((columns[0] + columns[1])) / 2;
+            columns[1] = Math.floor((columns[0] + columns[1]) / 2);
         } else if (letter == 'L') {
-            columns[0] = (int) Math.ceil((columns[0] + columns[1])) / 2;
+            columns[0] = Math.ceil((columns[0] + columns[1]) / 2);
         }
     }
 
     private static void addToArray() {
-        int rowID = (row * 8) + column;
+        double rowID = (row * 8) + column;
 
         computedResult.add(rowID);
     }
