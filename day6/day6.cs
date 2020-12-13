fab416682604a05514dd0a0af1fee9504da2fb4b
Maxime <maxime.jme@gmail.com>
2020-12-13 01:13:01 +0100

    add day6 part one

---
+++ day6/day6.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace day6
+{
+    internal class Program
+    {
+        public static List<int> SumCount = new List<int>();
+        
+        public static void Main(string[] args)
+        {
+            Part2();
+
+            Console.WriteLine(SumCount.Sum());
+        }
+
+        public static void Part1()
+        {
+            string[] lines = File.ReadAllLines(@"input.txt", Encoding.UTF8);
+            List<char> seens = new List<char>();
+
+            foreach (string line in lines)
+            {
+                char[] q = line.ToCharArray();
+
+                if (line == lines.Last())
+                {
+                    SumCount.Add(seens.Count);
+                }
+                else if (line != "")
+                {
+                    for (int i = 0; i < q.Length; i++)
+                    {
+                        if (!seens.Contains(q[i]))
+                        {
+                            seens.Add(q[i]);
+                        }
+                    }    
+                }
+                else
+                {
+                    SumCount.Add(seens.Count);
+                    seens = new List<char>();
+                }
+            }
+        }
+
+        public static void Part2()
+        {
+            string[] lines = File.ReadAllLines(@"input.txt", Encoding.UTF8);
+            List<char> seens = new List<char>();
+
+            foreach (string line in lines)
+            {
+                char[] q = line.ToCharArray();
+
+                if (line == lines.Last())
+                {
+                    SumCount.Add(seens.Count);
+                }
+                else if (line != "")
+                {
+                    for (int i = 0; i < q.Length; i++)
+                    {
+                        if (!seens.Contains(q[i]))
+                        {
+                            seens.Add(q[i]);
+                        }
+                    }    
+                }
+                else
+                {
+                    SumCount.Add(seens.Count);
+                    seens = new List<char>();
+                }
+            }
+        }
+    }
+}
