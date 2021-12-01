4bee0d31af2b11e918060f78fc93f0b7902787d9
Kayoshi-dev <maxime.jme@gmail.com>
2021-12-01 19:26:54 +0100

    Add part2

--- 2021/day1/index.js
+++ 2021/day1/index.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 
+let lines;
+
 try {
     const data = fs.readFileSync('input.txt', 'UTF-8');
 
-    const lines = data.split(/\r?\n/);
+    lines = data.split(/\r?\n/);
 
+} catch (err) {
+    console.error(err);
+}
+
+const part1 = () => {
     let previous;
     let greater = 0;
 
     lines.forEach((value, index) => {
-        if(index !== 0) {
+        if (index !== 0) {
 
-            if(Number(value) > previous) {
-                console.log(value, previous)
+            if (Number(value) > previous) {
                 greater++;
             }
         }
@@ -21,6 +27,30 @@ try {
     });
 
     console.log(greater)
-} catch (err) {
-    console.error(err);
 }
+
+part1();
+
+// Trying some functional code lmao
+const part2 = () => {
+    let counter = 0;
+    let previous = 0;
+
+    lines
+        .map(v => Number(v))
+        .map((v, i, arr) => arr.slice(i, i + 3))
+        .filter(chunk => chunk.length === 3)
+        .map(chunk => chunk.reduce((prev, cur) => prev + cur))
+        .map((v, i) => {
+            // I'm too bad in functional programming sorry
+            if(i !== 0) {
+                if(v > previous) counter++;
+            }
+
+            previous = v;
+        });
+
+    console.log(counter);
+}
+
+part2();
