7d9f0aa6a584acb7172cbf674cea8c4abb506c1a
Kayoshi-dev <maxime.jme@gmail.com>
2022-12-02 12:20:46 +0100

    Add day2 part one

---
+++ 2022/day2/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+let lines;
+
+try {
+  const data = fs.readFileSync("input.txt", "utf-8");
+
+  lines = data.split(/\r?\n/);
+} catch (err) {
+  console.error(err);
+}
+
+const part1 = () => {
+  const sum = lines.reduce((acc, current) => {
+    const [elf, player] = current.replace(/[AX]+/g, 'A'.charCodeAt(0) - 64).replace(/[BY]+/g, 'B'.charCodeAt(0) - 64).replace(/[CZ]+/g, 'C'.charCodeAt(0) - 64).split(" ").map(el => parseInt(el, 10));
+
+    if (player === 1 && elf === 3) {
+      return acc + 6 + player;
+    }
+
+    if (player === 3 && elf === 1) {
+      return acc + player;
+    }
+
+    if(elf === player) {
+      console.log("Tie");
+      return acc + 3 + player;
+    }
+
+    if(elf > player) {
+      console.log("Elf wins");
+      return acc + player;
+    }
+
+    if(elf < player) {
+      console.log("Player wins");
+
+      return acc + 6 + player;
+    }
+  }, 0);
+
+  console.log(sum);
+}
+
+part1();
